acf366c8282a9d442a2220dec386f7b899f2dc56
Caio Arruda <caioarruda@gmail.com>
2025-10-25 12:29:36 -0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    port: 3000,
-    host: true
+    host: "::",
+    port: 3000
   },
   plugins: [
     react(),
